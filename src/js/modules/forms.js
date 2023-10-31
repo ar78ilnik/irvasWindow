adff5a39e79a74aa7e394166e370468b13ad67cb
ar78ilnik <ar78ilnik@gmail.com>
2023-10-31 10:44:35 +0300

    Отправка формы посредством AJAX и захват всех введенных данных. Опвещение пользователя о состоянии отправки. В поле телефон только цифры

---
+++ src/js/modules/forms.js
@@ -0,0 +1,70 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input'),
+        phoneInputs = document.querySelectorAll('input[type="user_phone"]');
+
+    phoneInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            // проверка ввода пользователя номера телефона
+            // D - все не цифры
+            item.value = item.value.replace(/\D/, '');
+        });
+    });
+
+    const message = {
+        loading: 'Load...',
+        succes: 'Thank you',
+        failure: 'Somethink is wrong'
+    };
+
+    const postData = async (url, data) => {
+        // с помощью этой функции мы скрываем реализацию. Fetch API нам возвращают промис и этот промис мы должны еще раз обработать
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+        // есть нюанс, fetch это асинхронная функция. Когда мы запустим postData, она выполнит первую строку, потом она выполнит запрос (определенные данные по определеннуму url). Далее выполняется следующий код (res). На данный момент res пустая. Функция не ждет ответа. Мы можем воспользоваться операторами ES7 async/await. Тем самым мы говорим функции что есть асинхронные операции. Перед параметрами мы пишем async, а перед fetch мы пишем await
+
+        // здесь мы обрабатываем промис. Здесь тоже есть проблема, потому что res.text() тоже выполняется асинхронно. В этом случае return пуст.
+        //return res.text();
+        //поэтому пишем
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+
+            //Здесь используем е потому что в ТЗ сказано, что данные должны уходить без перезагрузки страницы
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            // собираем данные, которые есть в форме
+            const formData = new FormData(item);
+
+            // написание запроса
+            postData('assets/server.php', formData).then(res => {
+                console.log(res);
+                statusMessage.textContent = message.succes;
+            }).catch(() => {
+                statusMessage.textContent = message.failure;
+            }).finally(() => {
+                clearInputs();
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 3000);
+            })
+        });
+    });
+};
+
+export default forms;
